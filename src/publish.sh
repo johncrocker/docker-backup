f1e9db488ba2b38ca82ad4628639114f96b7135e
John Crocker <crockerish@gmail.com>
2024-07-10 01:14:44 +0100

    Fixed bug with volume backup

---
+++ src/publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+version="$1"
+
+	if [ -z "$version" ]; then
+		version="dev"
+	fi
+
+docker buildx build . \
+	-t "crockerish/docker-backup:$version"
+
+docker push "crockerish/docker-backup:$version"
