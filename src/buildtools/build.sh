41005a04d499640d66e921b35a6f393488934275
John Crocker <crockerish@gmail.com>
2024-07-10 20:04:22 +0100

    Linted

--- src/buildtools/build.sh
+++ src/buildtools/build.sh
@@ -9,21 +9,20 @@ cd $HOME/src/docker-backup/src
 
 shellcheck ./backup/docker-backup.sh
 
-  	if [[ "$?" -eq 0 ]]; then
-		echo "Shellcheck successful."
-        else
-                exit
-        fi
+if [[ "$?" -eq 0 ]]; then
+	echo "Shellcheck successful."
+else
+	exit
+fi
 
-shfmt ./backup/docker-backup.sh > ./backup/docker-backup.new
+shfmt ./backup/docker-backup.sh >./backup/docker-backup.new
 
-        if [[ "$?" -eq 0 ]]; then
-                mv ./backup/docker-backup.new ./backup/docker-backup
-        else
-                rm ./backup/docker-backup.new
-		exit
-        fi
+if [[ "$?" -eq 0 ]]; then
+	mv ./backup/docker-backup.new ./backup/docker-backup
+else
+	rm ./backup/docker-backup.new
+	exit
+fi
 
 docker buildx build . \
 	-t "crockerish/docker-backup:$version"
-
