2a0557736293284d114373dffd7a056e985b6ab8
John Crocker <crockerish@gmail.com>
2024-07-13 06:18:09 +0100

    simplified cron

---
+++ src/backup/docker-backup-cron.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+BACKUPDATE="$(date '+%Y%m%d-%H%M')"
+LOGFILENAME="/logs/docker-backup-""$BACKUPDATE"".log"
+
+	if [ ! -f "$LOGFILENAME" ]; then
+		echo "" >"$LOGFILENAME"
+	fi
+
+cd /app
+./docker-backup.sh >&1 | tee "$LOGFILENAME"
