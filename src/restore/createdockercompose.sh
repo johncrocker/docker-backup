e8f15cecb0a7adaef0a4d2d728a80eea914c542c
John Crocker <crockerish@gmail.com>
2024-07-16 02:43:11 +0100

    composer

--- src/restore/createdockercompose.sh
+++ src/restore/createdockercompose.sh
@@ -4,4 +4,18 @@
 # shellcheck disable=SC2002 # Useless cat. Consider cmd < file | .. or cmd file | .. instead.
 
 json="$(</dev/stdin)"
-echo "$json"
+
+printf "services:\n"
+printf "  %s:\n" $(echo "$json" | jq .[].Name -r | cut -b2-)
+printf "    image: %s\n" $(echo "$json" | jq .[].Config.Image -r)
+printf "    container_name: %s\n" $(echo "$json" | jq .[].Name -r | cut -b2-)
+printf "    hostname: %s\n" $(echo "$json" | jq .[].Config.Hostname -r)
+printf "    restart: %s\n" $(echo "$json" | jq .[].HostConfig.RestartPolicy.Name -r)
+printf "    labels:\n"
+echo "$json" | jq '.[].Config.Labels | keys[] as $key | [$key,.[$key]] | @tsv' -r | awk '!/^(org.opencontainers|com.docker)/{printf ("      - \"%s=%s\"\n" ,$1,$2)}'
+
+printf "\nvolumes:\n"
+printf "  {}\n"
+
+printf "\nnetworks:\n"
+printf "  {}\n"
