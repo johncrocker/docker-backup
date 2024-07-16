edc5a5af51ec1f3ec5ddbc419b7ee79d201d6cb8
John Crocker <crockerish@gmail.com>
2024-07-16 01:37:04 +0100

    restore

---
+++ src/restore/createdockercompose.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2317 # Don't warn about unreachable commands in this file
+# shellcheck disable=SC1090 # Can't follow non-constant source. Use a directive to specify location
+# shellcheck disable=SC2002 # Useless cat. Consider cmd < file | .. or cmd file | .. instead.
+
+json="$(</dev/stdin)"
+echo "$json"
---
+++ src/restore/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2317 # Don't warn about unreachable commands in this file
+# shellcheck disable=SC1090 # Can't follow non-constant source. Use a directive to specify location
+# shellcheck disable=SC2002 # Useless cat. Consider cmd < file | .. or cmd file | .. instead.
+
+docker container inspect mariadb | ./createdockercompose.sh
