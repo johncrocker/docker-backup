a15a5defa9754260ee315b0c6e461f432d56a07e
John Crocker <crockerish@gmail.com>
2024-07-17 16:13:03 +0100

    bugfix

--- src/buildtools/build.sh
+++ src/buildtools/build.sh
@@ -2,7 +2,7 @@
 version="$1"
 
 if [ -z "$version" ]; then
-	version="dev"
+	version="devbuild"
 fi
 
 cd $HOME/src/docker-backup/src
@@ -42,5 +42,7 @@ shfmt ./backup/docker-backup.sh > ./build/docker-backup.sh
 shfmt ./backup/docker-backup-cron.sh > ./build/docker-backup-cron.sh
 shfmt ./entrypoint.sh > ./build/entrypoint.sh
 
-docker buildx build . \
-	-t "crockerish/docker-backup:$version"
+docker buildx create --name multiarchbuilder --use --bootstrap
+docker buildx build . --load --platform linux/arm64 -t "crockerish/docker-backup:$version" | less -r
+docker buildx rm multiarchbuilder
+
